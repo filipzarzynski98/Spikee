09580c09a71a3d9ff588e1859d03ae04b4441162
filipzarzynski98 <zarzynskifilip@gmail.com>
2022-12-23 16:24:18 +0100

    some changes in Footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,13 +7,18 @@ const Header = () => {
         <div className='header'>
             <img className='header__logo' src={logo} alt="logo"></img>
             <ul className='header__nav-list'>
-                <li><strong>Contact</strong></li>
-                <li>FAQ</li>
-                <li>Regulamin</li>
-                <li>Rodo</li>
+                <li><strong>About us</strong></li>
+                <li>Contact</li>
+                <li>Terms</li>
+                <li>Privacy</li>
             </ul>
             <div className='header__login-and-menu__wrapper'>
-                <h3 className='login'>Login</h3>
+                <div className='login__wrapper'>
+                    <h3 className='login'>Log in</h3>
+                </div>
+                <div className='login__wrapper'>
+                    <h3 className='login'>Sign in</h3>
+                </div>
                 <div className='menu__wrapper'>
                     <div className='menu-squeres__top'>
                         <div className='squere'></div>
@@ -23,7 +28,6 @@ const Header = () => {
                         <div className='squere'></div>
                         <div className='squere'></div>
                     </div>
-                    Menu
                 </div>
             </div>
         </div>
